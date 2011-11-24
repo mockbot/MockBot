e9e7c8994c32c786fd1531f14b6ff32cd60ec40e
Robot Operating System <ros@ros.(none)>
2011-11-24 20:42:41 +0100

    Mapping VEL_CMD to Servo1 Servo2 added

--- MockBotVelocity2Servo/build/CMakeFiles/CMakeDirectoryInformation.cmake
+++ MockBotVelocity2Servo/build/CMakeFiles/CMakeDirectoryInformation.cmake
@@ -11,6 +11,31 @@ SET(CMAKE_FORCE_UNIX_PATHS 1)
 # The C and CXX include file search paths:
 SET(CMAKE_C_INCLUDE_PATH
   "../include"
+  "/opt/ros/electric/stacks/ros_comm/messages/std_msgs/include"
+  "/opt/ros/electric/stacks/ros_comm/messages/std_msgs/msg_gen/cpp/include"
+  "/opt/ros/electric/stacks/common_msgs/nav_msgs/msg_gen/cpp/include"
+  "/opt/ros/electric/stacks/common_msgs/nav_msgs/srv_gen/cpp/include"
+  "/opt/ros/electric/stacks/common_msgs/geometry_msgs/msg_gen/cpp/include"
+  "/opt/ros/electric/stacks/ros_comm/tools/rosbag/include"
+  "/opt/ros/electric/stacks/ros_comm/tools/topic_tools/include"
+  "/opt/ros/electric/stacks/ros_comm/tools/topic_tools/srv_gen/cpp/include"
+  "/opt/ros/electric/stacks/ros_comm/tools/rostest/include"
+  "/opt/ros/electric/stacks/ros_comm/clients/cpp/roscpp/include"
+  "/opt/ros/electric/stacks/ros_comm/clients/cpp/roscpp/msg_gen/cpp/include"
+  "/opt/ros/electric/stacks/ros_comm/clients/cpp/roscpp/srv_gen/cpp/include"
+  "/opt/ros/electric/stacks/ros_comm/clients/cpp/roscpp_serialization/include"
+  "/opt/ros/electric/stacks/ros_comm/clients/cpp/roscpp_traits/include"
+  "/opt/ros/electric/stacks/ros_comm/utilities/xmlrpcpp/src"
+  "/opt/ros/electric/stacks/ros_comm/tools/rosconsole/include"
+  "/opt/ros/electric/stacks/ros_comm/utilities/rostime/include"
+  "/opt/ros/electric/stacks/ros_comm/utilities/cpp_common/include"
+  "/opt/ros/electric/stacks/ros_comm/messages/rosgraph_msgs/msg_gen/cpp/include"
+  "/opt/ros/electric/ros/core/roslib/msg_gen/cpp/include"
+  "/opt/ros/electric/ros/core/roslib/include"
+  "/opt/ros/electric/ros/tools/rospack"
+  "/opt/ros/electric/ros/tools/rospack/include"
+  "/home/ros/prog/ros/hg/rosserial_msgs/msg_gen/cpp/include"
+  "/home/ros/prog/ros/hg/rosserial_msgs/srv_gen/cpp/include"
   )
 SET(CMAKE_CXX_INCLUDE_PATH ${CMAKE_C_INCLUDE_PATH})
 SET(CMAKE_Fortran_INCLUDE_PATH ${CMAKE_C_INCLUDE_PATH})
