0b81143412e0dfc40cf47547876c489398bd2934
root <root@ros.(none)>
2013-05-29 16:38:11 +0200

    permission bug at install script fixed octomap added

--- install_ros_groovy.sh
+++ install_ros_groovy.sh
@@ -1,4 +1,5 @@
-echo Install and setup ROS Groovy
+#!/bin/bash
+echo Install and setup ROS Groovy for $SUDO_USER 
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
 
@@ -14,10 +15,10 @@ sudo apt-get install ros-groovy-desktop-full
 sudo rosdep init
 rosdep update
 
-grep -i "/opt/ros/groovy/setup.bash" ~/.bashrc
+grep -i "/opt/ros/groovy/setup.bash" $HOME/.bashrc
 if [ $? -eq 1 ] ; then 
 	echo ROS Env updated
-	echo "source /opt/ros/groovy/setup.bash" >> ~/.bashrc
+	echo "source /opt/ros/groovy/setup.bash" >> $HOME/.bashrc
 else
 	echo ROS Env OK
 fi
@@ -31,11 +32,11 @@ fi
 
 source ~/.bashrc
 
-grep -i "ROS_PACKAGE_PATH=.:/opt/ros/prog:/opt/ros/groovy/share:/opt/ros/groovy/stacks" ~/.bashrc
+grep -i "ROS_PACKAGE_PATH=.:/opt/ros/prog:/opt/ros/groovy/share:/opt/ros/groovy/stacks" $HOME/.bashrc
 if [ $? -eq 1 ] ; then
 	echo ROS_PACKAGE_PATH modified
-	echo ROS_PACKAGE_PATH=.:/opt/ros/prog:/opt/ros/groovy/share:/opt/ros/groovy/stacks >> ~/.bashrc
-	source ~/.bashrc
+	echo ROS_PACKAGE_PATH=.:/opt/ros/prog:/opt/ros/groovy/share:/opt/ros/groovy/stacks >> $HOME/.bashrc
+	source $HOME/.bashrc
 else
 	echo ROS_PACKAGE_PATH OK
 fi
@@ -46,8 +47,9 @@ echo Adding additional packages
 sudo apt-get install ros-groovy-audio-common ros-groovy-openni-launch ros-groovy-openni-tracker ros-groovy-rosserial-arduino ros-groovy-rosserial-python ros-groovy-rosbridge-suite ros-groovy-mjpeg-server ros-groovy-mjpegcanvasjs ros-groovy-tf2-web-republisher
 
 sudo apt-get install apache2 libapache2-mod-php5 php5-curl mysql-server php5-mysql
-sudo apt-get install ros-groovy-freiburg-tools
+sudo apt-get install ros-groovy-freiburg-tools ros-groovy-octomap-mapping
 sudo apt-get install ros-groovy-hector-slam ros-groovy-hector-models ros-groovy-hector-worldmodel
 
-
-sudo chown -R $USER:$USER ~/.ros
+echo set permissions for user $SUDO_USER
+sudo chmod -R 777 $HOME/.ros
+sudo chown -R ros:ros $HOME/.ros
