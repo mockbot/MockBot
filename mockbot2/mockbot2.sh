255a4af3fb08c5b72586b55575478f93332c7ad6
Christian Mock <cmock@CMockPro-6.local>
2013-06-02 23:27:56 +0200

    verify connected hardware

--- mockbot2/mockbot2.sh
+++ mockbot2/mockbot2.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+/opt/ros/groovy/setup.bash
+
 echo Running MockBot2
-#/usr/bin/flite -t "running mockbot 2"
+/usr/bin/flite -t "running mockbot 2"
+
+#verify connected hardware
+
+# simulator only
+# kinect found
+
 roslaunch mockbot2 mockbot2.launch
 
