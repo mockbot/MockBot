e25b70201ece93e03eb48f862971b1f45e398968
root <root@daw.(none)>
2013-09-17 22:22:45 +0200

    RemoteROS.sh fixed for stupid environments

--- RemoteROS.sh
+++ RemoteROS.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
-export ROS_IP=$1
-export ROS_HOSTNAME=$1
-export ROS_MASTER_URI=http://$1:11311
-echo Using ROS at $1
+export ROS_HOSTNAME=`/bin/hostname -I`
+export ROS_MASTER_URI="http://$1:11311"
+echo Using ROS MASTER at $ROS_MASTER_URI from $ROS_HOSTNAME
 ping -c 1 $1
 rostopic list
+export LIBGL_ALWAYS_SOFTWARE=1
 
 
