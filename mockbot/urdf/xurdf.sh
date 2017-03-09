ec4e21cdd1f31398218b1031ab7ba20bf5a9e2fb
ros <christian.mock.chrimo@googlemail.com>
2017-03-09 01:01:30 +0100

    URDF updated for mockturtlebot

--- mockbot/urdf/xurdf.sh
+++ mockbot/urdf/xurdf.sh
@@ -1,6 +1,10 @@
-export target="mockbot.urdf"
-rosrun xacro xacro.py $target.xacro -o $target
-rosrun urdfdom check_urdf $target
-rosrun urdfdom urdf_to_graphiz $target
+export target="mockturtlebot.urdf"
+rosrun xacro xacro -i -o $target $target.xacro 
+check_urdf $target
+urdf_to_graphiz $target
 
+sleep 3
 
+#cat $target
+
+echo "http://mymodelrobot.appspot.com"
